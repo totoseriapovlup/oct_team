e9079718c5618714e0259bfb6eac73f9846d0c46
totoseriapovlup <tototester123456@gmail.com>
2025-02-20 19:35:30 +0200

    qwerty

--- test.php
+++ test.php
@@ -4,6 +4,6 @@ $login = $_POST['login'];
 
 $db = new mysqli('localhost', 'root', '', 'oct_2002');
 
-$sql="SELECT * FROM users WHERE login like '$login';";
+$sql="SELECT * FROM users WHERE login = '$login';";
 
 $db->query($sql);
