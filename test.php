0e5efa591433d6441f700710de03d5636eef5491
toto <test@test.com>
2025-02-20 18:46:46 +0200

    test reformat

--- test.php
+++ test.php
@@ -4,6 +4,6 @@ $login = $_POST['login'];
 
 $db = new mysqli('localhost', 'root', '', 'oct_2002');
 
-$sql="SELECT * FROM users WHERE login = '$login';";
+$sql = "SELECT * FROM users WHERE login = '$login';";
 
 $db->query($sql);
